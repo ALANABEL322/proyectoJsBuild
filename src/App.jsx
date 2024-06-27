e2f0feed5a88edc314673146744b1efb95e4778a
Alan <alanabelpereyra322@gmail.com>
2024-06-27 17:25:46 -0300

    ADD-Form-Vehicle-snippet

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,12 @@
+// eslint-disable-next-line no-unused-vars
 import React from "react";
-import CardRoute from "./CardRoute";
-
+import FormVehicle from "./FormVehicle";
 function App() {
-  const routes = [
-    {
-      startingPoint: "Obelisco de Buenos Aires",
-      branch: "Sucursal Microcentro",
-      vehicle: "MMB-125",
-      endingPoint: "Terminal Retiro",
-    },
-    {
-      startingPoint: "Puerto Madero",
-      branch: "Sucursal Palermo",
-      vehicle: "OPR-202",
-      endingPoint: "Aeropuerto Ezeiza",
-    },
-    {
-      startingPoint: "Plaza de Mayo",
-      branch: "Sucursal Recoleta",
-      vehicle: "AB-343-CI",
-      endingPoint: "Estadio Monumental",
-    },
-  ];
+
 
   return (
     <div className="flex flex-col items-center">
-      {routes.map((route, index) => (
-        <CardRoute
-          key={index}
-          startingPoint={route.startingPoint}
-          branch={route.branch}
-          vehicle={route.vehicle}
-          endingPoint={route.endingPoint}
-        />
-      ))}
+      <FormVehicle />
     </div>
   );
 }
---
+++ src/FormVehicle.jsx
@@ -0,0 +1,109 @@
+const FormVehicle = () => {
+  return (
+    <div className="flex justify-center p-4">
+      <div className="flex w-full md:w-3/5 lg:w-3/4 bg-base-100  m-4">
+        <div className="w-2/5 flex flex-col justify-start">
+          <img
+            className="object-contain w-full h-auto rounded-l-md"
+            src="https://res.cloudinary.com/dn5ltihzv/image/upload/v1719519366/imagenes/veh%C3%ADculo.png"
+            alt="image-vehicle"
+          />
+        </div>
+        <form className="w-3/5 p-6 flex flex-col">
+          <div className="flex flex-col w-full">
+            <div className="w-full mb-4 shadow-xl rounded-md">
+              <label className="text-lg text-[#00294f] font-bold mb-2">
+                Modelo
+              </label>
+              <input
+                type="text"
+                className="w-full p-[20px] focus:outline-none"
+                placeholder="Modelo"
+              />
+            </div>
+            <div className="w-full mb-4 shadow-xl rounded-md">
+              <label className="text-lg text-[#00294f] font-bold mb-2">
+                Capacidad
+              </label>
+              <input
+                type="text"
+                className="w-full p-[20px] focus:outline-none"
+                placeholder="Capacidad"
+              />
+            </div>
+            <div className="w-full mb-4 shadow-xl rounded-md flex space-x-4">
+              <div className="w-1/2">
+                <label className="text-lg text-[#00294f] font-bold mb-2">
+                  Placa
+                </label>
+                <input
+                  type="text"
+                  className="w-full p-[20px] focus:outline-none"
+                  placeholder="Placa"
+                />
+              </div>
+              <div className="w-1/2">
+                <label className="text-lg text-[#00294f] font-bold mb-2">
+                  Kilometraje
+                </label>
+                <input
+                  type="text"
+                  className="w-full p-[20px] focus:outline-none"
+                  placeholder="Kilometraje"
+                />
+              </div>
+            </div>
+            <div className="w-full mb-4 shadow-xl rounded-md">
+              <label className="text-lg text-[#00294f] font-bold mb-2">
+                Chofer
+              </label>
+              <input
+                type="text"
+                className="w-full p-[20px] focus:outline-none"
+                placeholder="Chofer"
+              />
+            </div>
+            <div className="w-full mb-4 shadow-xl rounded-md">
+              <label className="text-lg text-[#00294f] font-bold mb-2">
+                GPS del Vehículo
+              </label>
+              <input
+                type="text"
+                className="w-full p-[20px] focus:outline-none"
+                placeholder="GPS del Vehículo"
+              />
+            </div>
+            <div className="w-full mb-4 shadow-xl rounded-md">
+              <label className="text-lg text-[#00294f] font-bold mb-2">
+                Contraseña
+              </label>
+              <input
+                type="password"
+                className="w-full p-[20px] focus:outline-none"
+                placeholder="Contraseña"
+              />
+            </div>
+            <div className="w-full mb-4 shadow-xl rounded-md">
+              <label className="text-lg text-[#00294f] font-bold mb-2">
+                Sucursal
+              </label>
+              <input
+                type="text"
+                className="w-full p-[20px] focus:outline-none"
+                placeholder="Sucursal"
+              />
+            </div>
+          </div>
+          <button
+            type="submit"
+            className="p-2 rounded-md mt-3 bg-blue-700 text-white"
+          >
+            Registrar
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default FormVehicle;

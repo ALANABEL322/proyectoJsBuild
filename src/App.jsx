c13ee0b2850c466e47ae4a45d578ed9016b8630a
Alan <alanabelpereyra322@gmail.com>
2024-07-07 19:38:38 -0300

    add-CardSistemaDeRutas

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,19 @@
 // eslint-disable-next-line no-unused-vars
 import React from "react";
-import CardSucursal from "./cardSucursal";
-// import FormVehicle from "./FormVehicle";
+// import CardSucursal from "./cardSucursal";
+// import FormVehicle from "./formVehicle";
+// import FormSucursal from "./formSucursal";
 // import CardCar from "./cardCar";
+import CardSistemaDeRutas from "./cardSistemaDeRutas";
 
 function App() {
   return (
     <div className="flex flex-col items-center">
       {/* <CardCar /> */}
+      <CardSistemaDeRutas />
+      {/* <FormSucursal /> */}
       {/* <FormVehicle /> */}
-      <CardSucursal />
+      {/* <CardSucursal /> */}
     </div>
   );
 }
---
+++ src/formSucursal.tsx
@@ -0,0 +1,91 @@
+// import React from "react";
+
+// const FormSucursal = () => {
+//   return (
+//     <div className="flex justify-center p-4">
+//       <div className="flex flex-col md:flex-row w-full md:w-3/5 lg:w-3/4 bg-base-100 m-4 rounded-md">
+//         <div className="w-full md:w-2/5 flex flex-col justify-start">
+//           <img
+//             className="object-contain w-full h-auto rounded-t-md md:rounded-l-md md:rounded-t-none"
+//             src="https://res.cloudinary.com/dn5ltihzv/image/upload/v1720374369/imagenes/Imagen%20sucursal.png"
+//             alt="image-vehicle"
+//           />
+//         </div>
+//         <form className="w-full md:w-3/5 p-6 flex flex-col">
+//           <div className="flex flex-col w-full">
+//             <div className="w-full mb-4 shadow-xl rounded-md">
+//               <label className="text-lg text-[#00294f] font-bold mb-2">
+//                 Nombre
+//               </label>
+//               <input
+//                 type="text"
+//                 className="w-full p-[20px] focus:outline-none  border-gray-300 rounded-md"
+//                 placeholder="Nombre"
+//               />
+//             </div>
+//             <div className="w-full mb-4 shadow-xl rounded-md">
+//               <label className="text-lg text-[#00294f] font-bold mb-2">
+//                 Dirección
+//               </label>
+//               <input
+//                 type="text"
+//                 className="w-full p-[20px] focus:outline-none  border-gray-300 rounded-md"
+//                 placeholder="Dirección"
+//               />
+//             </div>
+//             <div className="w-full mb-4 shadow-xl rounded-md flex flex-col md:flex-row space-x-0 md:space-x-4">
+//               <div className="w-full md:w-1/2 mb-4 md:mb-0">
+//                 <label className="text-lg text-[#00294f] font-bold mb-2">
+//                   País
+//                 </label>
+//                 <input
+//                   type="text"
+//                   className="w-full p-[20px] focus:outline-none  border-gray-300 rounded-md"
+//                   placeholder="País"
+//                 />
+//               </div>
+//               <div className="w-full md:w-1/2">
+//                 <label className="text-lg text-[#00294f] font-bold mb-2">
+//                   Gerente
+//                 </label>
+//                 <input
+//                   type="text"
+//                   className="w-full p-[20px] focus:outline-none  border-gray-300 rounded-md"
+//                   placeholder="Gerente"
+//                 />
+//               </div>
+//             </div>
+//             <div className="w-full mb-4 shadow-xl rounded-md">
+//               <label className="text-lg text-[#00294f] font-bold mb-2">
+//                 Ciudad
+//               </label>
+//               <input
+//                 type="text"
+//                 className="w-full p-[20px] focus:outline-none border-gray-300 rounded-md"
+//                 placeholder="Ciudad"
+//               />
+//             </div>
+//             <div className="w-full mb-4 shadow-xl rounded-md">
+//               <label className="text-lg text-[#00294f] font-bold mb-2">
+//                 Codigo
+//               </label>
+//               <input
+//                 type="text"
+//                 className="w-full p-[20px] focus:outline-none  border-gray-300 rounded-md"
+//                 placeholder="Codigo"
+//               />
+//             </div>
+//           </div>
+//           <button
+//             type="submit"
+//             className="p-2 rounded-md mt-3 bg-blue-700 text-white"
+//           >
+//             Registrar
+//           </button>
+//         </form>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default FormSucursal;

bcc846df58c81972c4444027a35a77bc1cebd138
Alan <alanabelpereyra322@gmail.com>
2024-07-07 22:28:40 -0300

    ADD-DropDownButton

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,18 @@
 // eslint-disable-next-line no-unused-vars
 import React from "react";
+import DropdownButton from "./dropdownButton";
 // import CardSucursal from "./cardSucursal";
 // import FormVehicle from "./formVehicle";
 // import FormSucursal from "./formSucursal";
 // import CardCar from "./cardCar";
-import CardSistemaDeRutas from "./cardSistemaDeRutas";
+// import CardSistemaDeRutas from "./cardSistemaDeRutas";
 
 function App() {
   return (
     <div className="flex flex-col items-center">
+      <DropdownButton />
       {/* <CardCar /> */}
-      <CardSistemaDeRutas />
+      {/* <CardSistemaDeRutas /> */}
       {/* <FormSucursal /> */}
       {/* <FormVehicle /> */}
       {/* <CardSucursal /> */}

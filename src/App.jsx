1b421aeaa460f2be5c401b5b939ac851005d93e9
Alan <alanabelpereyra322@gmail.com>
2024-07-13 19:59:10 -0300

    initial template with login/auth snippet

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,14 @@
 // eslint-disable-next-line no-unused-vars
 import React from "react";
-// import ComponenteAuth from "./componenteAuth";
-// import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import LoginAdmin from "./loginAdmin";
-// import LoginChofer from "./loginChofer";
-// import InventarioCard from "./inventarioCard";
-// import Inventario from "./inventario";
-// import Paqueteria from "./paqueteria";
-// import DropdownButton from "./dropdownButton";
-// import CardSucursal from "./cardSucursal";
-// import FormVehicle from "./formVehicle";
-// import FormSucursal from "./formSucursal";
-// import CardCar from "./cardCar";
-// import CardSistemaDeRutas from "./cardSistemaDeRutas";
+
 
 function App() {
-  // const data = [
-  //   { name: "Page A", uv: 4000, pv: 2400, amt: 2400 },
-  //   { name: "Page B", uv: 3000, pv: 1398, amt: 2210 },
-  //   { name: "Page C", uv: 2000, pv: 9800, amt: 2290 },
-  //   { name: "Page D", uv: 2780, pv: 3908, amt: 2000 },
-  //   { name: "Page E", uv: 1890, pv: 4800, amt: 2181 },
-  //   { name: "Page F", uv: 2390, pv: 3800, amt: 2500 },
-  //   { name: "Page G", uv: 3490, pv: 4300, amt: 2100 },
-  // ];
+ 
 
   return (
     <div className="flex flex-col items-center">
-      {/* <ComponenteAuth /> */}
       <LoginAdmin />
-      {/* <LoginChofer /> */}
-      {/* <InventarioCard /> */}
-      {/* <Inventario data={data} /> */}
-      {/* <Paqueteria /> */}
-      {/* <DropdownButton /> */}
-      {/* <CardCar /> */}
-      {/* <CardSistemaDeRutas /> */}
-      {/* <FormSucursal /> */}
-      {/* <FormVehicle /> */}
-      {/* <CardSucursal /> */}
     </div>
   );
 }
